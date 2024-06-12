7d06f35e06d06e1c07d2e01d8d7d4c5ebb1fcd4b
Samyak Shah <samyakshah3008@gmail.com>
2024-06-12 22:03:27 +0530

    projects update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Laptop on desk with coffee cup and plant.";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Blog Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is to showcase my blog posts and articles which I have written after my learnings in tech industry. ",
+    url: "https://samyakshah-portfolio.netlify.app/blogs",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Honest Emoji",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Know your emotions. This will interpret emojis. Put an emoji and it will tell what that means.",
+    url: "https://know-emotions.netlify.app/",
   },
   {
-    title: "My Resume Site",
+    title: "Ferb Latin Speak",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Made a translation web app that will translate your sentences into Ferb-Latin language.",
+    url: "https://ferb-latin-translator-bysamyak.netlify.app/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Horror Movie Recommendor",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Made a ReactJS recommendation app : Horror Universe in which there are top movies,series and books recommended",
+    url: "https://horror-universe.netlify.app/",
   },
 ];
 
